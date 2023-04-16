5646ead7a640a2b497ddca9c09f6578aae410aae
ArmyofDeux <103246856+Emmanuetoks@users.noreply.github.com>
2023-04-16 05:05:28 -0700

    Made the home page responsive
    modified the css stylesheets to make the homepage responsive on tablet and mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,39 @@
 import React from 'react'
 
 const Navbar = () => {
+
+  let shouldDisplay = 'false';
+
+  const toggleMenu = ()=>{
+    let navMenu = document.getElementById('navMenu');
+    navMenu.classList.toggle('nav__menu--mob-display');
+    console.log(navMenu.getAttribute('display'));
+  }
+
+
   return (
-    <nav className='nav flex-item'>
+    <nav id='navBar' className='nav flex'>
         <img className='nav__logo' src='data/images/assets/shared/logo.svg'/>
-        <ul className='nav__menu flex-item pos-rel ff-barlow '>
-          <li className='nav__list-item'>
+        <a onClick={toggleMenu} href='#' className='nav__hamburger'>
+          <img src='/data/images/assets/shared/icon-hamburger.svg'/>
+        </a>
+        <ul id='navMenu' display={shouldDisplay} className='nav__menu flex pos-rel ff-barlow '>
+          <li className='nav__list-item flex pos-rel'>
             <a href='#' className='nav__link pos-rel'>
                 <b>00 </b> Home
             </a>
           </li>
-          <li className='nav__list-item'>
+          <li className='nav__list-item flex pos-rel'>
             <a href='#' className='nav__link pos-rel'>
               <b>01 </b>Destination
             </a>
           </li>
-          <li className='nav__list-item'>
+          <li className='nav__list-item flex pos-rel' >
             <a href='#' className='nav__link pos-rel'>
               <b>02 </b> Crew
             </a>
           </li>
-          <li className='nav__list-item'>
+          <li className='nav__list-item flex pos-rel'>
             <a href='#' className='nav__link pos-rel'>
               <b>03 </b>Technology
             </a>
